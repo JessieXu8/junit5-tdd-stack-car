1047c819f9027edaea44bca80aca9198c7fb6384
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-11 17:28:22 +0800

    [jessie] completed the test of 3

--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -1,18 +1,32 @@
 package com.thoughtworks.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
     private int size;
+
+    Map<Receipt,Car> list= new HashMap<>();
+
     public ParkingLot(int size) {
         this.size = size;
     }
 
-    public void park(Car car) {
+    public Receipt park(Car car) {
         if (size == 0){
             throw new ParkingLotFullException();
         }
+        Receipt receipt =new Receipt();
+        list.put(receipt, car);
+        return receipt;
     }
 
-    public String unPark(Receipt receipt) {
-        return null;
+    public Car unPark(Receipt receipt) {
+//        try {
+//            return list.);
+//        }catch (ParkingLotFullException e){
+//
+//        }
+        return list.remove(receipt);
     }
 }
