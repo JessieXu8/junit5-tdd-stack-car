4779ec6baea8a2eeeff6a2fae7ccb0900cc82271
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-16 21:54:17 +0800

    [jessie] completed the test of ParkingController

--- src/test/java/com/thoughtworks/tdd/ParkingControllerTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ParkingControllerTest {
@@ -24,5 +26,23 @@ public class ParkingControllerTest {
         when(parkingBoy.isFull()).thenReturn(false);
         String currentStatus=parkingController.getParkStatus(parkingBoy);
         assertThat(currentStatus,is("parkPage"));
+    }
+    
+    @Test
+    public void should_park_successful_when_call_getParkCommandController(){
+        ParkingController parkingController = new ParkingController();
+        String command = "N12344";
+        Car car =new Car(command);
+        Response response =mock(Response.class);
+        ParkingBoy parkingBoy=mock(ParkingBoy.class);
+        Receipt receipt =mock(Receipt.class);
+        when(parkingBoy.park(any())).thenReturn(receipt);
+        when(receipt.getReceipt()).thenReturn("123");
+//        when(response.send("")).thenReturn()
+        ParkingView view = mock(ParkingView.class);
+        String currentStatus = parkingController.getParkCommandController(command,parkingBoy);
+//        assertThat(currentStatus,is("parkingLotMainPage"));
+      verify(view).displayParkSuccessfull(any());
+//        verify(response).send("");
     }
 }
